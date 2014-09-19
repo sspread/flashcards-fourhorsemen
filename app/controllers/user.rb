4eb0c22efe2bba674071fdfced1eebaaa08efd73
Neel Shah <nshah197@yahoo.com>
2014-09-19 17:28:20 -0500

    profile

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,3 +1,14 @@
 #user.rb
 
 # all login/logout/authentication routing
+
+before 'user/profile/:id' do
+	unless current_user
+		redirect '/login'
+	end
+end
+
+get 'user/profile' do
+	@user = User.find(session[user_id])
+	erb :"users/profile"
+end
