e101013b9d9b31f5fc6f083495bc01c13b8f8cdd
Sam Spread <sspread2@gmail.com>
2014-09-19 17:22:03 -0500

    Working on card logic

--- db/migrate/20140919150641_create_rounds.rb
+++ db/migrate/20140919150641_create_rounds.rb
@@ -2,7 +2,7 @@ class CreateRounds < ActiveRecord::Migration
   def change
     create_table :rounds do |t|
       t.integer :user_id, :deck_id
-      t.integer :correct_count, :incorrect_count
+      t.integer :correct_count, :incorrect_count, default: 0
 
       t.timestamps
     end
