73a37a53d61f9a74ab52ce93117f82d213516235
Sam Spread <sspread2@gmail.com>
2014-09-19 19:54:17 -0500

    Core logic works TODO - logout button beautify profile with table and statistics general styling, consistency accross pages.

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -4,11 +4,32 @@ get '/decks' do
 end
 
 get '/decks/:deck_id' do
-  @round = Round.create(user_id: session[:user_id], deck_id: params[:deck_id])
+
+  round = Round.create(user_id: session[:user_id], deck_id: params[:deck_id])
+  session[:round_id] = round.id
+  session[:card_index] = 0
+  session[:card_ids_arr] = round.deck.card_ids
   erb :'deck/card'
   # redirect "/decks/#{}"
 end
 
-post '/decks/:deck_id/cards/:card_id' do
-
+post '/decks/cards' do
+  if session[:card_index] < session[:card_ids_arr].length-1
+    round = Round.find(session[:round_id])
+    card = Card.find(session[:card_ids_arr][session[:card_index]])
+    @guess = params[:guess]
+    if card.answer == params[:guess]
+      @correct = "right"
+      round.correct_count += 1
+      round.save
+    else
+      @correct = "dead wrong"
+      round.incorrect_count += 1
+      round.save
+    end
+    session[:card_index] += 1
+    erb :'deck/card'
+  else
+    erb :'users/profile'
+  end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,3 @@
 class Card < ActiveRecord::Base
-  has_many :decks
+  belongs_to :deck
 end
